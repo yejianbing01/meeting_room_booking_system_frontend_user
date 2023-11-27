e585f18bf23e7ca22680b2901215985d71f64089
yejianbin <yejb_01@163.com>
2023-11-27 14:21:02 +0800

    关闭 webpack-dev-server overlay runtimes error

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const {
+    overrideDevServer,
+    watchAll
+} = require("customize-cra");
+
+module.exports = {
+    devServer: overrideDevServer(
+        // dev server plugin
+        watchAll(),
+        (config) => {
+            config.client.overlay = false;
+            return config
+        }
+    )
+};
